5fc6ca2df7f9694b5576fb94ac1ac2ac1ce66436
NoNameA 774 <nonamea.774@gmail.com>
2016-07-20 02:16:31 +0900

    initial commit

---
+++ gulp/config.js
@@ -0,0 +1,39 @@
+// @file config.js
+var dest = './build'; // 出力先ディレクトリ
+var src = './src';  // ソースディレクトリ
+
+module.exports = {
+  // 出力先の指定
+  dest: dest,
+
+  // jsのビルド設定
+  js: {
+    src: src + '/js/**',
+    dest: dest + '/js',
+    uglify: false
+  },
+
+  // webpackの設定
+  webpack: {
+    entry: src + '/js/app.js',
+    output: {
+      filename: 'bundle.js'
+    },
+//    devtool: 'inline-source-map',
+    module: {
+      loaders: [
+        { test: /\.jsx$/, loader: 'jsx-loader' }
+      ]
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    }
+  },
+
+  copy: {
+    src: [   // 今後ただコピーするファイルが増えそうなので配列にしておく
+      src + '/html/index.html'
+    ],
+    dest: dest
+  }
+}
