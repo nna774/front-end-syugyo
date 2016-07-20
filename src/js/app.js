78411b9cb9e34f4642b7871552050d8d0cfc1550
NoNameA 774 <nonamea.774@gmail.com>
2016-07-20 15:17:27 +0900

    const

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,9 @@ var Hello = require('./components/Hello');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+const root = 'root';
+
 ReactDOM.render(
   <Hello />,
-  document.getElementById('root')
+  document.getElementById(root)
 );
